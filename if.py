3bf523678d352e6f963b127c41cb938634fdf8ff
felix <kyeum14@gmail.com>
2020-10-07 16:43:23 +1000

    Create if.py

---
+++ if.py
@@ -0,0 +1,48 @@
+#   " # " this allow us to write a comment. use this 
+
+#Example-1 
+mom_question = input("Felix! is it rainning outside ?")
+# this is valriable and it takes answer and store into mom_question
+
+
+# Q1. write the what u studied by using ' # ' commenting. 
+if (mom_question == "yes") :
+    print("mom you need to bring an umbrella ")
+#WRITE ANSWER BELOW 
+=> 
+elif (mom_question == "no"):
+    print("mom you dont't need to bring an umb")
+else:
+    print("Felix! you should asnwer me 'yes' or 'nope!!!'")
+
+
+#Q2 " HOW TO EXECUTE  if.py in the terminal ?"
+=>
+
+# Q3 write the code using ( == != < > <= >= )
+
+
+# See the code below and answer by commenting this has THREE question to answer. Q4 , Q5 , Q6 , Q7
+
+#Eg.  QX. User input int 45 which letter the program return ? 
+#F
+
+ if score >= 90:
+        letter = 'A'
+
+    elif score >= 80:
+        letter = 'B'
+    elif score >= 70:
+        letter = 'C'
+    elif score >= 60:
+        letter = 'D'
+    else:
+        letter = 'F'
+
+#Q4 User input int 45 which letter the program return ?
+
+#Q5 User input int 80 which letter the program return ?
+
+#Q6 User input int 75 which letter the program return ?
+
+#Q7 User input int 1 which letter the program return ?
