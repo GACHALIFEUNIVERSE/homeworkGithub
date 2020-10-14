56ee9b087f74c768ffb48571b0c6fb341b101727
felix <kyeum14@gmail.com>
2020-10-14 17:14:15 +1000

    Update if.py

--- if.py
+++ if.py
@@ -1,55 +1,13 @@
-#   " # " this allow us to write a comment. use this 
+points = input ("how many points do you have?")
 
-# to put a sentence or word so it can explain the program you made
-
-#Example-1 
-mom_question = input("Felix! is it rainning outside ?")
-# this is valriable and it takes answer and store into mom_question
-
-
-# Q1. write the what u studied by using ' # ' commenting. 
-if (mom_question == "yes") :
-    print("mom you need to bring an umbrella ")
-#WRITE ANSWER BELOW 
-
-elif (mom_question == "no"):
-    print("mom you dont't need to bring an umb")
+if (points>=100):
+    print ("you have reached the level 50") 
+elif (points==50):
+    print("you have to start at level 35")
+elif (points < 5):
+     print ("well done the level you have been given was level 250")
+elif (points == 12):
+     print("sorry the level that you have been given is level 5")
 else:
-    print("Felix! you should asnwer me 'yes' or 'nope!!!'")
-
-
-#Q2 " HOW TO EXECUTE  if.py in the terminal ?"
-#python if.py
-
-# Q3 write the code using ( == != < > <= >= )
-#print(9==9) => true
-#print(9!=9) => false
-#print(2<10) => true
-#print(2>10) => false > ( greater than )
-#print(5<=10)=> true
-#print(9>=11)=> false
-# See the code below and answer by commenting this has THREE question to answer. Q4 , Q5 , Q6 , Q7
-
-#Eg.  QX. User input int 45 which letter the program return ? 
-#F
-
-# if score >= 90:
- #       letter = 'A'
-
-  #  elif score >= 80:
-   #     letter = 'B'
-    #elif score >= 70:
-     #   letter = 'C'
-    #elif score >= 60:
-     #   letter = 'D'
-    #else:
-     #   letter = 'F'
-
-#Q4 User input int 45 which letter the program return ?
-=> F
-#Q5 User input int 80 which letter the program return ?
-=> b
-#Q6 User input int 75 which letter the program return ?
-=> f 
-#Q7 User input int 1 which letter the program return ?
-=> f
+    print ("error")
+  
